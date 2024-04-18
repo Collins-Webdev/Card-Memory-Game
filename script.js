a437e1652380d73ad2ababea46f38878af6a3d85
AkramMoussaoui <akrammoussaoui14@gmail.com>
2024-04-18 23:08:10 +0200

    Create dynamic card grid

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 const cards = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h"
-  ];
-  
-  // Function to shuffle the cards array
-  function shuffleCards(cards) {
-    for (let i = cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [cards[i], cards[j]] = [cards[j], cards[i]];
-    }
+  "done_outline",
+  "expand_circle_down",
+  "download_2",
+  "highlight_text_cursor",
+  "arrows_outward",
+  "all_match",
+  "left_panel_open",
+  "clock_loader_90",
+  "done_outline",
+  "expand_circle_down",
+  "download_2",
+  "highlight_text_cursor",
+  "arrows_outward",
+  "all_match",
+  "left_panel_open",
+  "clock_loader_90",
+];
 
+const container = document.querySelector(".container");
+
+// Function to shuffle the cards array
+function shuffleCards(cards) {
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
   }
-  
-  // Shuffling the cards array
-  const shuffledCards = shuffleCards(cards);
-  
-  // Creating the card elements with the shuffled cards
-  const cardElements = shuffledCards.map(card => `
-    <div class="card">
-      <img src="${card}" />
-    </div>
-  `);
+}
+
+// Shuffling the cards array
+shuffleCards(cards);
+
+// Creating the card elements with the shuffled cards
+const cardElements = cards.map(
+  (card) => `
+    <span class="material-symbols-outlined"> ${card} </span>
+  `
+);
+
+// Put the card on the page
+cardElements.forEach((element) => {
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = element;
+  container.appendChild(div);
+});
